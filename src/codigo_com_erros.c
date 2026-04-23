8cb63912c6f494683d94bf24949f71eed3dd3bd6
thorflk <thorfcamargo21@gmail.com>
2026-04-23 19:34:02 -0300

    Adicionado ; após as variáveis

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // <> adicionados
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; // ; adicionado
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
