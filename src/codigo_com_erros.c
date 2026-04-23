c1ed6af46a90a80fef555f42e616306d4e575aa4
thorflk <thorfcamargo21@gmail.com>
2026-04-23 19:40:18 -0300

    Adicionado & antes da variável nota2

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); // adicionado & antes da variavel
 
     media = (nota1 + nota2) / 2; // adicionado parenteses
 
