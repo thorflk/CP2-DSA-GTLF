ff324f57d7b3b1c6966cb04efd54dc597432c68e
GabrielRoma0 <gabrielbromao@gmail.com>
2026-04-23 20:00:39 -0300

    Variáveis do código adicionadas e verificação se média já foi calculada

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main() {
+    int opcao;
+    float nota_1 , nota_2 , media;
+    int a, b, c;
+    
+    // Variável para controlar se a média já foi calculada
+    int calculou_media = 0; 
+
     
 
     
