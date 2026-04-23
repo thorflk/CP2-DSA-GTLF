6a4490942cdb24b021dfc0a3e12e5b5a56863bcf
GabrielRoma0 <gabrielbromao@gmail.com>
2026-04-23 20:05:57 -0300

    Estrutura do-while e menu de escolhas adicionada

--- src/main.c
+++ src/main.c
@@ -5,11 +5,32 @@ int main() {
     float nota_1 , nota_2 , media;
     int a, b, c;
     
-    // Variável para controlar se a média já foi calculada
+     // Variável para controlar se a média já foi calculada
     int calculou_media = 0; 
 
+do { // Estrutura "do-while adicionada"
+        printf("\n==============================\n");
+        printf(" SISTEMA EQUIPE GTLF\n");
+        printf("==============================\n");
+        printf("1 - Inserir notas\n");
+        printf("2 - Calcular media\n");
+        printf("3 - Verificar situacao\n");
+        printf("4 - Exibir resultado\n");
+        printf("5 - Calcular derivada\n");
+        printf("6 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        
+        
     
 
-    
-    return 0;
-}
+
+
+
+
+
+while(opcao != 6);
+    }
+return 0;
+
+} 
